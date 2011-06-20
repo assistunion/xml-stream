15340259c3f76b3e09bd011be293f5efe8752031
dimituri <dimituri@gmail.com>
2011-06-20 23:19:02 +0300

    Inherited XmlStream from EventEmitter; introduced `match[: selector]' event

--- test/foo2.js
+++ test/foo2.js
@@ -2,12 +2,12 @@ var XmlStream = require('../lib/xml-stream');
 var fs = require('fs');
 
 var xml = new XmlStream();
-xml.on('channel > title', function(item, context, trace) {
+xml.on('match: channel > title', function(item, context, trace) {
   console.log(item);
   console.log(context.rss.channel.title == context.channel.title);
   console.log(trace);
 });
-xml.on('rss', function(rss) {
+xml.on('match: rss', function(rss) {
   console.log(rss['@']['xmlns:atom']);
 });
 xml.parse(fs.createReadStream(__dirname + '/pandalog.xml'));
