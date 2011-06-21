a18d15d8d63304aee10567e284117ab02797fe88
dimituri <dimituri@gmail.com>
2011-06-21 07:21:20 +0300

    Added example for stream replacements

---
+++ examples/foo3.js
@@ -0,0 +1,14 @@
+var XmlStream = require('../lib/xml-stream');
+var fs = require('fs');
+
+var xml = new XmlStream(fs.createReadStream(__dirname + '/pandalog.xml'));
+xml.on('startElement: item', function(node) {
+  node.name = 'cake';
+  node.element.$.foo = 'bar';
+});
+xml.on('text: item > description', function(node) {
+  node.text = node.text.replace(/^[^:]+:\s+/, '');
+});
+xml.on('data', function(data) {
+  process.stdout.write(data);
+});
