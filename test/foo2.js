e471b828cde67a11391a5cd66093d3bfdcfdb3f1
dimituri <dimituri@gmail.com>
2011-06-21 03:39:32 +0300

    Switched to startElement, endElement, and text events instead of match

--- test/foo2.js
+++ test/foo2.js
@@ -2,11 +2,14 @@ var XmlStream = require('../lib/xml-stream');
 var fs = require('fs');
 
 var xml = new XmlStream(fs.createReadStream(__dirname + '/pandalog.xml'));
-xml.on('match: channel > title', function(item, context, trace) {
+xml.on('endElement: channel > title', function(item, context, trace) {
   console.log(item);
   console.log(context.rss.channel.title == context.channel.title);
   console.log(trace);
 });
-xml.on('match: rss', function(rss) {
+xml.on('endElement: rss', function(rss) {
   console.log(rss['@']['xmlns:atom']);
+});
+xml.on('text: channel > title', function(item) {
+  console.log(item);
 });
