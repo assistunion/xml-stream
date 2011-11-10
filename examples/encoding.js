6ef3c3b7e31d0e3448bef764640d2d811a8a78a9
dimituri <dimituri@gmail.com>
2011-11-10 16:10:53 +0200

    Added input encoding detection from XML declaration

--- examples/encoding.js
+++ examples/encoding.js
@@ -7,20 +7,15 @@ var fs        = require('fs')
 function setup(encoding) {
   var stream = fs.createReadStream(path.join(__dirname, 'encoding.xml'));
   var xml = new XmlStream(stream, encoding);
+  xml.on('endElement: node', function(node) {
+    console.log(node);
+  });
+  xml.on('error', function(message) {
+    console.log('Parsing as ' + (encoding || 'auto') + ' failed: ' + message);
+  });
   return xml;
 }
 
-// Try to parse as UTF-8.
-var xml = setup('utf8');
-xml.on('endElement: node', function(node) {
-  console.log(node);
-});
-xml.on('error', function(message) {
-  console.log('Parsing as UTF-8 failed: ' + message);
-});
-
-// Try to parse as ISO 8859-5.
-var xml = setup('iso-8859-5');
-xml.on('endElement: node', function(node) {
-  console.log(node);
-});
+var xml = setup('utf8');       // Parse as UTF-8
+var xml = setup('iso-8859-5'); // Parse as ISO 8859-5
+var xml = setup();             // Detect on the fly.
